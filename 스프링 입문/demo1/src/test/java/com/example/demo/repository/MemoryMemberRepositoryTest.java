847b8e10c1caa2f4b7ffc295d29ef7ee0c2e9438
okane <okanekudasai6@gmail.com>
2023-09-20 03:00:24 +0900

    safsef

---
+++ 스프링 입문/demo1/src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        Assertions.assertThat(member1).isEqualTo(result);
+    }
+}
